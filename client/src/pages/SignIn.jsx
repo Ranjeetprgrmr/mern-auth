5c3c32c7b6817bcee1640f14ea6ed47de73b763c
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-08 11:18:27 +0545

    add routes and create pagess

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,10 @@
+    import React from 'react'
+    
+    export default function SignIn() {
+      return (
+        <div className='text-red-500'>
+          Sing in page
+        </div>
+      )
+    }
+    
