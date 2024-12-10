20e169650eee7d48f35810b93b3ad7d3859030a7
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-10 18:58:03 +0545

    update header component and make profile picture

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  const location = useLocation();
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -11,14 +12,18 @@ export default function Header() {
         <ul className="flex gap-4 font-semibold">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          {location.pathname !== "/" && location.pathname !== "/sign-in" && (
-            <Link to="/sign-in">Sign In</Link>
-          )}
-
-          {location.pathname !== "/" && location.pathname !== "/sign-up" && (
-            <Link to="/sign-up">Sign Up</Link>
-          )}
-          <Link to="/profile">Profile</Link>
+          <Link to="/profile">
+          {currentUser ? (
+            <img src={currentUser.profilePicture} alt="profile"
+            className="h-7 w-7 rounded-full object-cover"
+             />
+          ) : (
+            <li>Sign In</li>
+          )
+          }
+          </Link>
+          {/* <Link to="/sign-up">Sign Up</Link>
+          <Link to="/profile">Profile</Link> */}
         </ul>
       </div>
     </div>
